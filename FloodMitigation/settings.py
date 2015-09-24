2af15e17c1de771688956f8020d71a2bb8e33649
Nick Santos <nick.r.santos@gmail.com>
2015-09-24 13:02:39 -0700

    Changed to spatialite, added basic models

--- FloodMitigation/settings.py
+++ FloodMitigation/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'relocation',
 )
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'FloodMitigation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
--- relocation/models.py
+++ relocation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Region(models.Model):
+	boundary_polygon = models.FileField()
+	pass
+
+class Constraint(models.Model):
+	"""
+
+	"""
+	enabled = models.BooleanField(default=True)
+	name = models.TextField(max_length=255)
+	description = models.TextField()
+	module_name = models.TextField()  # must match the name of a module in "constraints" module - module must have a class named "Constraint"
