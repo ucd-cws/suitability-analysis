943cf17307e9e2a903f09adb78e60d2b889e52ae
Nick Santos <nickrsan@users.noreply.github.com>
2015-10-24 15:11:32 -0700

    Actual 1.8.5 compatibility fix

--- FloodMitigation/settings.py
+++ FloodMitigation/settings.py
@@ -105,5 +105,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATES = {'TEMPLATE_DIRS': (os.path.join(BASE_DIR,  'templates'),)
-			 }
+TEMPLATES = [
+        {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],  # For those who wants to have a custom place for templates in their Django apps/projects.
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
