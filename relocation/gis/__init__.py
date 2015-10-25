d9bd7ad23320a0aef4958527918297e359c8191e
Nick Santos <nickrsan@users.noreply.github.com>
2015-10-24 18:12:56 -0700

    Added environment variable toggle, finished land use conversion to code

--- relocation/gis/__init__.py
+++ relocation/gis/__init__.py
@@ -22,3 +22,24 @@ def create_working_directories(base_folder, folder_name):
 	workspace = os.path.join(working_directory, gdb_name)
 
 	return working_directory, workspace
+
+def store_environments(environments_list):
+	"""
+		Given an iterable of environment variables for arcpy, it stores the current values into a dictionary so they can be reset after an operation
+	:param environments_list:
+	:return:
+	"""
+	stored_environments = {}
+	for env in environments_list:
+		stored_environments[env] = arcpy.env.__dict__[env]
+
+	return stored_environments
+
+def reset_environments(stored_environments):
+	"""
+		Given a dict of stored environment variables (from store_environments), it resets the environment values to the value in the dictionary
+	:param stored_environments:
+	:return:
+	"""
+	for env in stored_environments.keys():
+		arcpy.env.__dict__[env] = stored_environments[env]
--- relocation/gis/land_use.py
+++ relocation/gis/land_use.py
@@ -6,6 +6,9 @@ import arcpy
 
 from code_library.common.geospatial import generate_gdb_filename
 
+from relocation.gis import store_environments, reset_environments
+from relocation.gis import filter_patches
+
 geoprocessing_log = logging.getLogger("geoprocessing")
 
 def make_road_mask(tiger_lines, census_places, search_area):
@@ -35,7 +38,7 @@ def make_road_mask(tiger_lines, census_places, search_area):
 	# run buffer on lines
 	geoprocessing_log.info("Buffering roads")
 	buffered_roads = generate_gdb_filename("buffered_roads", scratch=True)
-	arcpy.Buffer_analysis(tiger_clip,buffered_roads,field_name)
+	arcpy.Buffer_analysis(tiger_clip, buffered_roads, field_name)
 
 	# erase using census lines
 	geoprocessing_log.info("Removing census places from mask (adding back to banned areas")
@@ -46,220 +49,36 @@ def make_road_mask(tiger_lines, census_places, search_area):
 	return road_mask
 
 
-def land_use(nlcd_layer, search_area, tiger_lines, census_places):
+def land_use(nlcd_layer, search_area, tiger_lines, census_places, crs, workspace):
 	arcpy.CheckOutExtension("spatial")
 
 	geoprocessing_log.info("Extracting NLCD raster to search area")
 	nlcd_in_area = arcpy.sa.ExtractByMask(nlcd_layer, search_area)
 	thresholded_raster = nlcd_in_area > 24 | nlcd_in_area == 21  # TODO: find a way to make this a parameter - an explicit list of banned values?
 
+	stored_environments = store_environments(('cellSize', 'mask', 'extent', 'snapRaster'))  # cache the env vars so we can reset them at the end of this function
+	arcpy.env.cellSize = nlcd_in_area
+	arcpy.env.mask = nlcd_in_area
+	arcpy.env.extent = nlcd_in_area
+	arcpy.env.snapRaster = nlcd_in_area
 
-# Script arguments
-NLCD_Land_Cover_Layer = arcpy.GetParameterAsText(0)
-if NLCD_Land_Cover_Layer == '#' or not NLCD_Land_Cover_Layer:
-    NLCD_Land_Cover_Layer = "X:\\Flood_Mitigation\\GIS\\Suitability_Data.gdb\\nlcd_2001_land_cover" # provide a default value if unspecified
+	roads_mask = make_road_mask(tiger_lines, census_places=census_places, search_area=search_area)
+	roads_raster = generate_gdb_filename("roads_raster")
+	geoprocessing_log.info("Converting roads mask to raster")
+	arcpy.PolygonToRaster_conversion(roads_mask, "OBJECTID", roads_raster, "CELL_CENTER", cellsize=nlcd_in_area)
 
-Tiger_Lines_for_Same_Year = arcpy.GetParameterAsText(1)
-if Tiger_Lines_for_Same_Year == '#' or not Tiger_Lines_for_Same_Year:
-    Tiger_Lines_for_Same_Year = "tiger_roads_2011" # provide a default value if unspecified
+	# Raster Calculations
+	final_nlcd = arcpy.sa.Con(arcpy.sa.IsNull(arcpy.sa.Raster(roads_raster)), thresholded_raster, 1)
+	intermediate_raster = generate_gdb_filename("intermediate_nlcd_mask")
+	projected_raster = generate_gdb_filename("projected_nlcd_mask", gdb=workspace)
+	final_nlcd.save(intermediate_raster)
 
-Census_Places_for_Same_Year = arcpy.GetParameterAsText(2)
-if Census_Places_for_Same_Year == '#' or not Census_Places_for_Same_Year:
-    Census_Places_for_Same_Year = "X:\\Flood_Mitigation\\GIS\\exploration\\suitability_dev\\suitability.gdb\\Census_Places_1990" # provide a default value if unspecified
+	reset_environments(stored_environments)
 
-Output_Coordinate_System = arcpy.GetParameterAsText(3)
-if Output_Coordinate_System == '#' or not Output_Coordinate_System:
-    Output_Coordinate_System = "PROJCS['NAD_1983_Contiguous_USA_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-96.0],PARAMETER['Standard_Parallel_1',29.5],PARAMETER['Standard_Parallel_2',45.5],PARAMETER['Latitude_Of_Origin',23.0],UNIT['Meter',1.0]]" # provide a default value if unspecified
+	geoprocessing_log.info("Reprojecting final raster")
+	arcpy.ProjectRaster_management(intermediate_raster, projected_raster, out_coor_system=crs)
 
-Alignment_Raster = arcpy.GetParameterAsText(4)
-if Alignment_Raster == '#' or not Alignment_Raster:
-    Alignment_Raster = "X:\\Flood_Mitigation\\GIS\\derived.gdb\\Flood_dem_10m_albers" # provide a default value if unspecified
+	filtered_nlcd_poly = filter_patches.convert_and_filter_by_code(projected_raster, filter_value=0)
 
-# Local variables:
-NLCD_Binary = NLCD_Land_Cover_Layer
-Buffered_Roads_Raster = NLCD_Binary
-Roads_Masked_Out = Buffered_Roads_Raster
-rastercalc2_ProjectRaster = Roads_Masked_Out
-Road_Mask = Census_Places_for_Same_Year
-TIGER = Tiger_Lines_for_Same_Year
-tl_2011_55113_roads_CopyFeat1 = TIGER
-With_Buffer_Field = TIGER
-tl_2011_55113_roads_CopyFeat = With_Buffer_Field
-
-# Process: Copy Features
-arcpy.CopyFeatures_management(Tiger_Lines_for_Same_Year, TIGER, "", "0", "0", "0")
-
-# Process: Add Field
-arcpy.AddField_management(TIGER, "Buffer_Distance", "LONG", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Calculate Field
-arcpy.CalculateField_management(With_Buffer_Field, "Buffer_Distance", "distance( !MTFCC!)", "PYTHON_9.3", "def distance(r_type):\\n  if r_type == \"S1100\":  # highways, or so\\n    return 128  # 3x cell size (30m) * sqrt(2) to capture diagonals\\n  elif r_type == \"S1200\":  # major roadways\\n    return 85  # 2x cell size (30m) * sqrt(2) to capture diagonals\\n  else:  #other\\n    return 43  # cell size (30m) * sqrt(2) to capture diagonals")
-
-# Process: Buffer
-arcpy.Buffer_analysis(TIGER, tl_2011_55113_roads_CopyFeat1, "Buffer_Distance", "FULL", "ROUND", "NONE", "", "PLANAR")
-
-# Process: Erase
-arcpy.Erase_analysis(tl_2011_55113_roads_CopyFeat1, Census_Places_for_Same_Year, Road_Mask, "")
-
-# Process: Filter out unbuildable types
-tempEnvironment0 = arcpy.env.newPrecision
-arcpy.env.newPrecision = "SINGLE"
-tempEnvironment1 = arcpy.env.autoCommit
-arcpy.env.autoCommit = "1000"
-tempEnvironment2 = arcpy.env.XYResolution
-arcpy.env.XYResolution = ""
-tempEnvironment3 = arcpy.env.XYDomain
-arcpy.env.XYDomain = ""
-tempEnvironment4 = arcpy.env.scratchWorkspace
-arcpy.env.scratchWorkspace = "E:\\Users\\dsx\\Documents\\ArcGIS\\Default.gdb"
-tempEnvironment5 = arcpy.env.cartographicPartitions
-arcpy.env.cartographicPartitions = ""
-tempEnvironment6 = arcpy.env.terrainMemoryUsage
-arcpy.env.terrainMemoryUsage = "false"
-tempEnvironment7 = arcpy.env.MTolerance
-arcpy.env.MTolerance = ""
-tempEnvironment8 = arcpy.env.compression
-arcpy.env.compression = "LZ77"
-tempEnvironment9 = arcpy.env.coincidentPoints
-arcpy.env.coincidentPoints = "MEAN"
-tempEnvironment10 = arcpy.env.randomGenerator
-arcpy.env.randomGenerator = "0 ACM599"
-tempEnvironment11 = arcpy.env.outputCoordinateSystem
-arcpy.env.outputCoordinateSystem = ""
-tempEnvironment12 = arcpy.env.rasterStatistics
-arcpy.env.rasterStatistics = "STATISTICS 1 1"
-tempEnvironment13 = arcpy.env.ZDomain
-arcpy.env.ZDomain = ""
-tempEnvironment14 = arcpy.env.transferDomains
-arcpy.env.transferDomains = "false"
-tempEnvironment15 = arcpy.env.resamplingMethod
-arcpy.env.resamplingMethod = "NEAREST"
-tempEnvironment16 = arcpy.env.projectCompare
-arcpy.env.projectCompare = "NONE"
-tempEnvironment17 = arcpy.env.cartographicCoordinateSystem
-arcpy.env.cartographicCoordinateSystem = "PROJCS['NAD_1983_Contiguous_USA_Albers',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Albers'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-96.0],PARAMETER['Standard_Parallel_1',29.5],PARAMETER['Standard_Parallel_2',45.5],PARAMETER['Latitude_Of_Origin',23.0],UNIT['Meter',1.0]]"
-tempEnvironment18 = arcpy.env.configKeyword
-arcpy.env.configKeyword = ""
-tempEnvironment19 = arcpy.env.outputZFlag
-arcpy.env.outputZFlag = "Same As Input"
-tempEnvironment20 = arcpy.env.qualifiedFieldNames
-arcpy.env.qualifiedFieldNames = "true"
-tempEnvironment21 = arcpy.env.tileSize
-arcpy.env.tileSize = "128 128"
-tempEnvironment22 = arcpy.env.parallelProcessingFactor
-arcpy.env.parallelProcessingFactor = ""
-tempEnvironment23 = arcpy.env.pyramid
-arcpy.env.pyramid = "PYRAMIDS -1 NEAREST DEFAULT 75 NO_SKIP"
-tempEnvironment24 = arcpy.env.referenceScale
-arcpy.env.referenceScale = ""
-tempEnvironment25 = arcpy.env.extent
-arcpy.env.extent = "DEFAULT"
-tempEnvironment26 = arcpy.env.XYTolerance
-arcpy.env.XYTolerance = ""
-tempEnvironment27 = arcpy.env.tinSaveVersion
-arcpy.env.tinSaveVersion = "CURRENT"
-tempEnvironment28 = arcpy.env.nodata
-arcpy.env.nodata = "NONE"
-tempEnvironment29 = arcpy.env.MDomain
-arcpy.env.MDomain = ""
-tempEnvironment30 = arcpy.env.spatialGrid1
-arcpy.env.spatialGrid1 = "0"
-tempEnvironment31 = arcpy.env.cellSize
-arcpy.env.cellSize = "MAXOF"
-tempEnvironment32 = arcpy.env.outputZValue
-arcpy.env.outputZValue = ""
-tempEnvironment33 = arcpy.env.outputMFlag
-arcpy.env.outputMFlag = "Same As Input"
-tempEnvironment34 = arcpy.env.geographicTransformations
-arcpy.env.geographicTransformations = "NAD_1927_To_NAD_1983_NADCON"
-tempEnvironment35 = arcpy.env.spatialGrid2
-arcpy.env.spatialGrid2 = "0"
-tempEnvironment36 = arcpy.env.ZResolution
-arcpy.env.ZResolution = ""
-tempEnvironment37 = arcpy.env.mask
-arcpy.env.mask = ""
-tempEnvironment38 = arcpy.env.spatialGrid3
-arcpy.env.spatialGrid3 = "0"
-tempEnvironment39 = arcpy.env.maintainSpatialIndex
-arcpy.env.maintainSpatialIndex = "false"
-tempEnvironment40 = arcpy.env.workspace
-arcpy.env.workspace = "E:\\Users\\dsx\\Documents\\ArcGIS\\Default.gdb"
-tempEnvironment41 = arcpy.env.MResolution
-arcpy.env.MResolution = ""
-tempEnvironment42 = arcpy.env.derivedPrecision
-arcpy.env.derivedPrecision = "HIGHEST"
-tempEnvironment43 = arcpy.env.ZTolerance
-arcpy.env.ZTolerance = ""
-arcpy.gp.RasterCalculator_sa("(\"%NLCD Land Cover Layer%\" > 24)  |  (\"%NLCD Land Cover Layer%\" == 21)", NLCD_Binary)
-arcpy.env.newPrecision = tempEnvironment0
-arcpy.env.autoCommit = tempEnvironment1
-arcpy.env.XYResolution = tempEnvironment2
-arcpy.env.XYDomain = tempEnvironment3
-arcpy.env.scratchWorkspace = tempEnvironment4
-arcpy.env.cartographicPartitions = tempEnvironment5
-arcpy.env.terrainMemoryUsage = tempEnvironment6
-arcpy.env.MTolerance = tempEnvironment7
-arcpy.env.compression = tempEnvironment8
-arcpy.env.coincidentPoints = tempEnvironment9
-arcpy.env.randomGenerator = tempEnvironment10
-arcpy.env.outputCoordinateSystem = tempEnvironment11
-arcpy.env.rasterStatistics = tempEnvironment12
-arcpy.env.ZDomain = tempEnvironment13
-arcpy.env.transferDomains = tempEnvironment14
-arcpy.env.resamplingMethod = tempEnvironment15
-arcpy.env.projectCompare = tempEnvironment16
-arcpy.env.cartographicCoordinateSystem = tempEnvironment17
-arcpy.env.configKeyword = tempEnvironment18
-arcpy.env.outputZFlag = tempEnvironment19
-arcpy.env.qualifiedFieldNames = tempEnvironment20
-arcpy.env.tileSize = tempEnvironment21
-arcpy.env.parallelProcessingFactor = tempEnvironment22
-arcpy.env.pyramid = tempEnvironment23
-arcpy.env.referenceScale = tempEnvironment24
-arcpy.env.extent = tempEnvironment25
-arcpy.env.XYTolerance = tempEnvironment26
-arcpy.env.tinSaveVersion = tempEnvironment27
-arcpy.env.nodata = tempEnvironment28
-arcpy.env.MDomain = tempEnvironment29
-arcpy.env.spatialGrid1 = tempEnvironment30
-arcpy.env.cellSize = tempEnvironment31
-arcpy.env.outputZValue = tempEnvironment32
-arcpy.env.outputMFlag = tempEnvironment33
-arcpy.env.geographicTransformations = tempEnvironment34
-arcpy.env.spatialGrid2 = tempEnvironment35
-arcpy.env.ZResolution = tempEnvironment36
-arcpy.env.mask = tempEnvironment37
-arcpy.env.spatialGrid3 = tempEnvironment38
-arcpy.env.maintainSpatialIndex = tempEnvironment39
-arcpy.env.workspace = tempEnvironment40
-arcpy.env.MResolution = tempEnvironment41
-arcpy.env.derivedPrecision = tempEnvironment42
-arcpy.env.ZTolerance = tempEnvironment43
-
-# Process: Polygon to Raster
-tempEnvironment0 = arcpy.env.snapRaster
-arcpy.env.snapRaster = "E:\\Users\\dsx\\Documents\\ArcGIS\\Default.gdb\\rastercalc1"
-tempEnvironment1 = arcpy.env.extent
-arcpy.env.extent = "E:\\Users\\dsx\\Documents\\ArcGIS\\Default.gdb\\rastercalc1"
-tempEnvironment2 = arcpy.env.cellSize
-arcpy.env.cellSize = "E:\\Users\\dsx\\Documents\\ArcGIS\\Default.gdb\\rastercalc1"
-arcpy.PolygonToRaster_conversion(Road_Mask, "OBJECTID", Buffered_Roads_Raster, "CELL_CENTER", "NONE", NLCD_Binary)
-arcpy.env.snapRaster = tempEnvironment0
-arcpy.env.extent = tempEnvironment1
-arcpy.env.cellSize = tempEnvironment2
-
-# Process: Raster Calculator
-arcpy.gp.RasterCalculator_sa("Con(IsNull(\"%Buffered_Roads_Raster%\"),\"%NLCD Binary%\", 1)", Roads_Masked_Out)
-
-# Process: Project Raster
-tempEnvironment0 = arcpy.env.snapRaster
-arcpy.env.snapRaster = "X:\\Flood_Mitigation\\GIS\\derived.gdb\\Flood_dem_10m_albers"
-tempEnvironment1 = arcpy.env.cellSize
-arcpy.env.cellSize = "X:\\Flood_Mitigation\\GIS\\derived.gdb\\Flood_dem_10m_albers"
-tempEnvironment2 = arcpy.env.mask
-arcpy.env.mask = "X:\\Flood_Mitigation\\GIS\\derived.gdb\\Flood_dem_10m_albers"
-arcpy.ProjectRaster_management(Roads_Masked_Out, rastercalc2_ProjectRaster, Output_Coordinate_System, "NEAREST", Alignment_Raster, "", "", "GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]")
-arcpy.env.snapRaster = tempEnvironment0
-arcpy.env.cellSize = tempEnvironment1
-arcpy.env.mask = tempEnvironment2
+	return filtered_nlcd_poly
 
