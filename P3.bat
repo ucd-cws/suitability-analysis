248b2bac60340466e431ab21c8c67bc7909ebc52
Nick Santos <nickrsan@users.noreply.github.com>
2016-07-13 15:33:04 -0700

    Updates related to migration to ArcGIS 10.3 - GDAL/OGR no longer a dependency

--- P3.bat
+++ P3.bat
@@ -1,1 +1,3 @@
-C:\Users\dsx.AD3\VirtualEnvs\FloodMitigationv2\Scripts\activate.bat
+"C:\Program Files\ArcGIS\Pro\bin\python\Scripts\proswap.bat" flood
+"C:\Program Files\ArcGIS\Pro\bin\python\Scripts\activate.bat" flood
+cd C:\Users\dsx.AD3\Code\FloodMitigation
--- relocation/gis/geojson.py
+++ relocation/gis/geojson.py
@@ -2,7 +2,6 @@ import os
 import logging
 
 import arcpy
-import ogr
 
 from relocation.gis import temp
 from FloodMitigation.settings import REPROJECTION_ID
@@ -19,6 +18,11 @@ def file_gdb_layer_to_geojson(geodatabase, layer_name, outfile):
 	geoprocessing_log.info("Reprojecting to web_mercator")
 	reprojected = temp.generate_gdb_filename(layer_name)
 	arcpy.Project_management(in_dataset=os.path.join(geodatabase, layer_name), out_dataset=reprojected, out_coor_system=arcpy.SpatialReference(REPROJECTION_ID))
+
+	geoprocessing_log.info("Writing out geojson file at {0:s}".format(reprojected))
+	arcpy.FeaturesToJSON_conversion(reprojected, outfile, geoJSON="GEOJSON")  # export GeoJSON with ArcGIS Pro
+
+	return  #  skip the code below for now, but retain it for legacy purposes for now. Can probably delete after August 2016. It was replaced by the line above
 
 	ogr.UseExceptions()
 
