063ef83ff81d7e8c5ab702ba2d5d000521dbfa33
Nick Santos <nickrsan@users.noreply.github.com>
2016-08-01 19:07:07 -0700

    Switched the river side code from model to Python - works now

--- relocation/gis/river_side.py
+++ relocation/gis/river_side.py
@@ -3,17 +3,47 @@ import os
 
 import arcpy
 
+from relocation.gis.temp import generate_gdb_filename
+
 from FloodMitigation.settings import BASE_DIR
 geoprocessing_log = logging.getLogger("geoprocessing_log")
 
 
-def mark_side_of_river(parcels, town_boundary, river):
+def mark_side_of_river(parcels, town_boundary, rivers):
 
-	geoprocessing_log.info("Importing Toolbox")
+	river_field = "is_river"
+	correct_side_field = "stat_is_on_correct_side_of_river"
+	dissolved_polygon_layer = "dissolved_polygon_layer"
+	parcels_layer = "parcels_layer"
 
-	arcpy.ImportToolbox(os.path.join(BASE_DIR, "relocation", "tbx", "river_side", "river_side.tbx"), "riverside")
+	# Process: Remove fields if they already exist - simpler
+	fields = [field.name for field in arcpy.ListFields(parcels)]
+	if river_field in fields:
+		arcpy.DeleteField_management(parcels, river_field)
+	if correct_side_field in fields:
+		arcpy.DeleteField_management(parcels, correct_side_field)
 
-	geoprocessing_log.info("Marking Parcels on the same side of the river")
-	arcpy.MarkParcelsOnSameSideOfRiver_riverside(town_boundary, river, parcels)  # function created by ImportToolbox
+	arcpy.AddField_management(parcels, river_field, "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+	arcpy.CalculateField_management(parcels, river_field, "0", "PYTHON_9.3", "")
 
-	geoprocessing_log.info("Parcels marked with river side")
+	# Process: Add Correct Side Field
+	arcpy.AddField_management(parcels, correct_side_field, "SHORT", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
+	arcpy.CalculateField_management(parcels, correct_side_field, "0", "PYTHON_9.3", "")
+
+	arcpy.MakeFeatureLayer_management(parcels, parcels_layer)
+	arcpy.SelectLayerByLocation_management(parcels_layer, "INTERSECT", rivers, "", "NEW_SELECTION", "NOT_INVERT")
+	arcpy.CalculateField_management(parcels_layer, "is_river", "1", "PYTHON_9.3", "")
+
+	arcpy.SelectLayerByLocation_management(parcels_layer, "INTERSECT", parcels_layer, "", "SWITCH_SELECTION", "NOT_INVERT")
+
+	dissolved_parcels = generate_gdb_filename("dissolved_parcels")
+	arcpy.Dissolve_management(parcels_layer, dissolved_parcels, "is_river", "", "SINGLE_PART", "DISSOLVE_LINES")
+
+	arcpy.MakeFeatureLayer_management(dissolved_parcels, dissolved_polygon_layer)
+
+	correct_side_on_disk = generate_gdb_filename("correct_side_on_disk")
+	arcpy.SelectLayerByLocation_management(dissolved_polygon_layer, "INTERSECT", town_boundary, "", "NEW_SELECTION", "NOT_INVERT")
+	arcpy.CopyFeatures_management(dissolved_polygon_layer, correct_side_on_disk)
+
+	arcpy.SelectLayerByLocation_management(parcels_layer, "INTERSECT", correct_side_on_disk, "", "NEW_SELECTION", "NOT_INVERT")
+	arcpy.CalculateField_management(parcels_layer, correct_side_field, "1", "PYTHON_9.3", "")
