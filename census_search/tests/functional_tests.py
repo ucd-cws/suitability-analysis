a31885efce6e6c7e1dbf5c73c79858525014d2fc
lilawren <li.lawrly@gmail.com>
2016-08-09 16:29:42 -0700

    Finished up unit tests and functional tests

--- census_search/tests/functional_tests.py
+++ census_search/tests/functional_tests.py
@@ -1,9 +1,9 @@
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-# assert 'Search test' in browser.title, "Browser title was " + browser.title
 
-class NewVisitorTest(unittest.TestCase):
+class NewSearchTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -12,14 +12,21 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_city_selection(self):
-		# Search
-		# homepage
+		# Search page
 		self.browser.get('http://localhost:8000/search')
 
-		# User notices the page title and header...
+		# User notices the Search bar...
 		self.assertIn('Census data', self.browser.title)
-		self.fail('Finish the test!')
+		search_text = self.browser.find_element_by_tag_name('label').text
+		self.assertIn('Places:', search_text)
 
+		# User enters something in the Search bar
+		place_input = self.browser.find_element_by_id('places')
+		place_input.send_keys('Cudahy,WI')
+
+		# User clicks View
+		link = self.browser.find_element_by_link_text('View')
+		link.click()
 
 
 if __name__ == '__main__':
--- census_search/tests/tests.py
+++ census_search/tests/tests.py
@@ -1,27 +1,33 @@
-from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from census_search.views import place_list, place_view
-
-from selenium import webdriver
 import arcpy
 from census_search.models import Place
 
+'''
+This is a unit test for the census_search app
 
+To run test do: python manage.py test census_search
 
-# assert 'Search test' in browser.title, "Browser title was " + browser.title
+'''
+
+# First features in the geodatabase
+first_city = "Columbus"
+first_state = "IL"
+first_key = "8063"
 
 class CensusSearchTest(TestCase):
 
-	# test load places on (current) first city, Cudahy, WI. Also places it in the test DB, "default"
-	def test_load_places(self):
-		print("Testing load_places for first feature")
+
+	# Adds only one object to the database for each test. Also tests load places on first city
+	def setUp(self):
+		print("\nLoading first feature into test database...")
 
 		rows = arcpy.SearchCursor(
 			"C:\\Users\\Lawrence\\Documents\\ArcGIS\\Projects\\census_search\\census_search.gdb\\city_states_joined",
 			fields="OBJECTID; NAME; STATENAME")
 
-		# Iterate through the rows in the cursor
+		# Get first row from the cursor
 		rowlist = list(rows)
 		row = rowlist[0]
 		p1 = Place()
@@ -29,34 +35,46 @@ class CensusSearchTest(TestCase):
 		p1.city = row.getValue("NAME")
 		p1.state = row.getValue("STATENAME")
 
-		filename = "city_" + str(p1.key)
+		global first_city, first_state, first_key
+		first_city = p1.city
+		first_state = p1.state
+		first_key = p1.key
+
+		print("First feature: " + "ID:" + str(first_key) + " " + first_city + " " + first_state)
+
+		filename = "city_" + str(first_key)
 		layer = "C:\\Users\\Lawrence\\PycharmProjects\\suitability-analysis\\census_search\\static\\geojson\\layers\\SBA city_states\\SBA.gdb\\" + filename
 
 		p1.layer = layer
-		# p1.geojson = "census_search/geojson/{0:s}/{1:d}.geojson".format(p1.static_folder, p1.key)  # Use this if do not want to recreate geojson files
-		#p1.as_geojson()  # create the gsojson file and set the attribute for its location
+		p1.geojson = "census_search/geojson/{0:s}/{1:d}.geojson".format(p1.static_folder, p1.key)  # Use this if do not want to recreate geojson files
+		# p1.as_geojson()  # create the gsojson file and set the attribute for its location
 		p1.save()
 
-
+		objs = Place.objects.all()
+		self.assertEqual(len(objs), 1)
 		print("Done.")
 
+
+	# test http://127.0.0.1:8000/search/view/ for first city and first state
+	def test_single_view(self):
+
+		print("\nTesting view of first feature...")
+		request = HttpRequest()
+		response = place_view(request, first_city, first_state)
+		self.assertTrue(response.content.startswith(b'\n\n<html>'))
+		self.assertIn(b'var layer = new L.geoJson.ajax("/static/census_search/geojson/cities/' + str.encode(str(first_key)) + b".geojson", response.content)  # check contents
+		print(first_city + ", " + first_state + " geojson file found")
+		self.assertTrue(response.content.endswith(b'</html>'))
+		print("SUCCESS.\n")
+
+
+	# test http://127.0.0.1:8000/search/
 	def test_search_page(self):
+
+		print("\nTesting search view...")
 		request = HttpRequest()
 		response = place_list(request)
 		self.assertTrue(response.content.startswith(b'\n\n<html>'))
 		self.assertIn(b'<div class="ui-widget col-md-5">', response.content) #check contents
 		self.assertTrue(response.content.endswith(b'</html>'))
-
-	def test_view_page(self):
-		request = HttpRequest()
-		response = place_view(request, "Cudahy", "WI")
-		self.assertTrue(response.content.startswith(b'\n\n<html>'))
-		self.assertIn(b'var layer = new L.geoJson.ajax("/static/census_search/geojson/cities/1.geojson").addTo(object_map);', response.content)  # check contents
-		self.assertTrue(response.content.endswith(b'</html>'))
-
-
-
-# test view for Cudahy, WI in city list view
-
-# test view for (current) first city, Cudahy, WI
-# http://127.0.0.1:8000/search/view/WI/Cudahy/
+		print("SUCCESS\n")
